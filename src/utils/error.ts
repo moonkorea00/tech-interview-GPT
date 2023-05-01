0aa4dc83d783fc33b7ca06eb7610802bdadabcf3
moonkorea00 <moonkorea00@gmail.com>
2023-05-02 00:43:14 +0900

    feat : options validator function

---
+++ src/utils/error.ts
@@ -0,0 +1,6 @@
+export const optionsErrMap = {
+  field: 'Development Field',
+  experience: 'Work Experience',
+  lang: 'Language',
+  api: 'OpenAI API Key',
+};
---
+++ src/utils/validateRequestOptions.ts
@@ -0,0 +1,42 @@
+import { options } from '@components/Main/Options/constants';
+import { optionsErrMap } from './error';
+
+const validateSearchParams = (searchParams: URLSearchParams) => {
+  const errors: string[] = [];
+  const queryKeys = Object.keys(options);
+
+  queryKeys.forEach(key => {
+    const value = searchParams.get(key);
+
+    if (!value || !Object.keys(options[key]).includes(value)) {
+      errors.push(optionsErrMap[key as keyof typeof optionsErrMap]);
+    }
+  });
+
+  return errors;
+};
+
+const validateState = (value: string) => {
+  const ERR_KEY = 'api';
+  const errors: string[] = [];
+  if (!value) errors.push(optionsErrMap[ERR_KEY]);
+
+  return errors;
+};
+
+export const validateRequestOptions = (
+  searchParams: URLSearchParams,
+  formValues: { [key: string]: string }
+) => {
+  const { apiKey } = formValues;
+
+  const searchParamsErrors = validateSearchParams(searchParams);
+  const stateErrors = validateState(apiKey);
+
+  const errors = [...searchParamsErrors, ...stateErrors];
+  const errorMessage = `Before we start, fill out the following options : ${errors?.join(
+    ', '
+  )}`;
+
+  return { error: errors.length > 0 ? errorMessage : '' };
+};
