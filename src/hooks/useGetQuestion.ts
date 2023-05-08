c99aac5bc095449763680c114691b7e3869b3770
moonkorea00 <moonkorea00@gmail.com>
2023-05-08 13:56:45 +0900

    feat : generate question when form is valid

--- src/hooks/useGetQuestion.ts
+++ src/hooks/useGetQuestion.ts
@@ -1,10 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from 'react';
+import { useEffect, Dispatch } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { questions } from '@utils/questions';
+import { Action } from '@@types/form';
 
-const useGetQuestion = () => {
-  const [question, setQuestion] = useState('');
+const useGetQuestion = (dispatch: Dispatch<Action>, condition: boolean) => {
   const [searchParams] = useSearchParams();
 
   const field = searchParams.get('field');
@@ -13,14 +13,15 @@ const useGetQuestion = () => {
   const handleGetQuestion = (arr: string[]) => {
     const randomQuestionIdx = Math.floor(Math.random() * arr.length);
     const randomQuestion = arr[randomQuestionIdx];
-    setQuestion(randomQuestion);
+    dispatch({ type: 'GET_QUESTION', payload: randomQuestion });
   };
 
-  useEffect(() => handleGetQuestion(questionsArr), []);
+  useEffect(() => {
+    if (!condition) return;
+    handleGetQuestion(questionsArr);
+  }, [condition]);
 
   return {
-    question,
-    setQuestion,
     handleGetQuestion: () => handleGetQuestion(questionsArr),
   };
 };
