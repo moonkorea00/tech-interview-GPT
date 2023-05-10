5a34315dadaa1936d4554771dddf8cf1d46b4a86
moonkorea00 <moonkorea00@gmail.com>
2023-05-10 10:40:12 +0900

    fix : check for query value on validate

--- src/utils/validateRequestOptions.ts
+++ src/utils/validateRequestOptions.ts
@@ -4,12 +4,14 @@ import { optionsErrMap } from './error';
 export const validateRequestOptions = (searchParams: URLSearchParams) => {
   const errors: string[] = [];
 
-  for (const [key, value] of searchParams) {
-    if (!options[key][value]) {
+  Object.keys(options).forEach(key => {
+    const value = searchParams.get(key);
+
+    if (!value || !Object.keys(options[key]).includes(value)) {
       errors.push(optionsErrMap[key as keyof typeof optionsErrMap]);
     }
-  }
-
+  });
+  
   if (errors.length > 0) {
     const errorMessage = `Before we start, fill out the following options : ${errors.join(
       ', '
