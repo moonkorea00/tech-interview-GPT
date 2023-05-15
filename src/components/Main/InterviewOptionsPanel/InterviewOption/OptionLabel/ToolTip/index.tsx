6f65307e21f6b294d27a3d9317a2e507834da5a1
moonkorea00 <moonkorea00@gmail.com>
2023-05-15 18:39:59 +0900

    feat : tooltip , OptionLabel Component chore : install html react parser

---
+++ src/components/Main/InterviewOptionsPanel/InterviewOption/OptionLabel/ToolTip/index.tsx
@@ -0,0 +1,20 @@
+import parse from 'html-react-parser';
+
+type OptionsTooltipProps = {
+  tooltipContent: string;
+};
+
+const OptionsTooltip = ({ tooltipContent }: OptionsTooltipProps) => {
+  return (
+    <div className="absolute bottom-full left-1/2 min-w-[240px] mb-2 transform -translate-x-1/2">
+      <div className="bg-[#555555] text-white text-xs px-2.5 py-2 rounded-lg whitespace-pre-line">
+        {parse(tooltipContent)}
+      </div>
+      <div className="absolute bottom-0 w-4 h-4 mx-auto inset-x-0">
+        <div className="w-3 h-4 bg-[#555555] transform rotate-45 origin-bottom-left"></div>
+      </div>
+    </div>
+  );
+};
+
+export default OptionsTooltip;
--- src/components/Main/constants/index.ts
+++ src/components/Main/constants/index.ts
@@ -1,8 +1,4 @@
-type Options = {
-  [key: string]: { [key: string]: string };
-};
-
-export const options: Options = {
+export const options = {
   field: {
     fe: 'Front-end',
     be: 'Back-end',
@@ -28,6 +24,8 @@ export const INTERVIEW_OPTIONS = [
     name: 'field',
     type: 'dropdown',
     options: options.field,
+    tooltipContent:
+      'Tech Interview GPT will ask questions relevant to your development field.',
   },
   {
     id: 2,
@@ -35,6 +33,8 @@ export const INTERVIEW_OPTIONS = [
     name: 'experience',
     type: 'dropdown',
     options: options.experience,
+    tooltipContent:
+      'Tech Interview GPT will provide feedback and follow up questions based on your work experience.',
   },
   {
     id: 3,
@@ -42,6 +42,15 @@ export const INTERVIEW_OPTIONS = [
     name: 'lang',
     type: 'dropdown',
     options: options.lang,
+    tooltipContent:
+      'Select the spoken language when converting speech to text.',
   },
-  { id: 4, label: 'OpenAI API Key', name: 'apiKey', type: 'input' },
+  {
+    id: 4,
+    label: 'OpenAI API Key',
+    name: 'apiKey',
+    type: 'input',
+    tooltipContent:
+      'Click <a href="https://platform.openai.com/account/api-keys" target="_blank" rel="noreferrer" style="color:#9FA6FC">here</a> to get your API key.\n\nDont have an OpenAI account? <a href="https://platform.openai.com/signup" target="_blank" rel="noreferrer" style="color:#9FA6FC">Create</a> a new account and get free trial tokens.\n\n Your API key will only be required when submitting your interview answer so feel free to try out the interview first.',
+  },
 ];
