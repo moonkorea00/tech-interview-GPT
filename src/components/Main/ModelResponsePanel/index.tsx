cf6191d90dfa4732d30012561b2fa15d3bd6672b
moonkorea00 <moonkorea00@gmail.com>
2023-05-15 22:13:29 +0900

    refactor : manage svgs in assets folder delete SVG components

--- src/components/Main/ModelResponsePanel/index.tsx
+++ src/components/Main/ModelResponsePanel/index.tsx
@@ -1,4 +1,4 @@
-import { FlickerSVG } from '../InterviewOptionsPanel/InterviewOption/Dropdown/Svg';
+import Flicker from '@assets/Main/Flicker.svg';
 import ChatGPT from '@assets/Main/ChatGPT.svg';
 import useTypingEffect from '../hooks/useTypingEffect';
 import { useFormSelector } from '@hooks/useFormContext';
@@ -13,7 +13,13 @@ const ModelResponsePanel = () => {
       <div className="flex flex-col justify-center w-full min-h-[50px] p-4 border border-border-default leading-6 rounded-md shadow-sectionInput">
         <span className="whitespace-pre-line">
           {isLoading ? 'loading' : modelMessage}
-          {(isAnimating || isLoading) && <FlickerSVG />}
+          {(isAnimating || isLoading) && (
+            <img
+              src={Flicker}
+              alt="flicker"
+              className="inline-block w-[10px] animate-flicker"
+            />
+          )}
         </span>
       </div>
     </section>
