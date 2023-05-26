fcf0a1aa7eb946956ef7202042a2669d72b15f33
moonkorea00 <moonkorea00@gmail.com>
2023-05-27 03:41:42 +0900

    feat : NavBar component

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Heading from '../Heading';
 import Nav from '../Nav';
+import NavBar from '../Nav/NavBar';
 
 const RootLayout = () => {
   return (
     <div className="flex w-full h-[100vh]">
       <Nav />
-      <div className="flex flex-col items-center w-full h-full gap-8 pt-20 overflow-y-auto">
+      <div className="flex flex-col items-center w-full h-full gap-8 py-24 xl:py-20 lg:pt-0 lg:pb-16 overflow-y-auto">
+        <NavBar />
         <Heading />
         <Outlet />
       </div>
---
+++ src/components/common/Nav/NavBar/index.tsx
@@ -0,0 +1,35 @@
+import { useState, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import NavList from '../NavList';
+import hamburger from '@assets/Nav/hamburger.svg';
+import useOnClickOutside from '@hooks/useOnClickOutside';
+
+const NavBar = () => {
+  const [isNavListVisible, setIsNavListVisible] = useState(false);
+  const navRef = useRef(null);
+
+  useOnClickOutside(navRef, () => setIsNavListVisible(false));
+
+  return (
+    <nav className="hidden lg:flex flex-col w-full" ref={navRef}>
+      <div className="flex justify-between items-center w-full h-[50px] px-4 bg-[#343541]">
+        <img
+          src={hamburger}
+          alt="menu"
+          className="h-[40px] py-1 hover:opacity-60 cursor-pointer"
+          onClick={() => setIsNavListVisible(prev => !prev)}
+        />
+        <Link to="/">
+          <span className="text-[#E4E7EB] text-4xl sm:text-3xl">&#43;</span>
+        </Link>
+      </div>
+      {isNavListVisible && (
+        <div className="flex flex-col items-start pt-1 pb-2 pl-5 text-[#E4E7EB] bg-[#343541]">
+          <NavList />
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default NavBar;
