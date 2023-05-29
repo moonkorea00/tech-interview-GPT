ecd3c5efefc3e6a6b163ac85bf1d0e94d54360fc
moonkorea00 <moonkorea00@gmail.com>
2023-05-29 09:37:59 +0900

    refactor : dynamically change tag of layout component

--- src/components/common/Layout/Main/index.tsx
+++ src/components/common/Layout/Main/index.tsx
@@ -1,14 +1,15 @@
 import type { ReactNode } from 'react';
 
-type MainLayoutProps = {
+type LayoutProps = {
   children: ReactNode;
+  tag?: keyof JSX.IntrinsicElements;
 };
 
-const MainLayout = ({ children }: MainLayoutProps) => {
+const MainLayout = ({ children, tag: LayoutTag = 'div' }: LayoutProps) => {
   return (
-    <div className="flex flex-col items-center w-[950px] xl:w-[730px] lg:w-[100%] lg:px-[40px] md:px-[20px] sm:px-[10px]">
+    <LayoutTag className="flex flex-col items-center w-[950px] xl:w-[730px] lg:w-[100%] lg:px-[40px] md:px-[20px] sm:px-[10px]">
       {children}
-    </div>
+    </LayoutTag>
   );
 };
 
