467f139f12e30cbdcd260956cbc53eca6fba5ad0
moonkorea00 <moonkorea00@gmail.com>
2023-05-30 11:23:35 +0900

    chore : vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 import Router from './Router';
 import './styles/globalStyles.css';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Router />
